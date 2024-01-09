80308df22e2bc1b8323157952e2ed5d7758bc4f5
Rakhal01234 <154105700+Rakhal01234@users.noreply.github.com>
2024-01-09 15:32:15 +0530

    Update Lab 2.java

--- Lab 2.java
+++ Lab 2.java
@@ -1,1 +1,58 @@
 
+import java.util.Scanner;
+
+class Book {
+    String name;
+    String author;
+    double price;
+    int numPages;
+
+    public Book(String name, String author, double price, int numPages) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.numPages = numPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book Details: \n" +
+                "Name: " + name + "\n" +
+                "Author: " + author + "\n" +
+                "Price: $" + price + "\n" +
+                "Number of Pages: " + numPages;
+    }
+}
+
+public class BookTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of books to create: ");
+        int n = scanner.nextInt();
+
+        Book[] books = new Book[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter details for Book " + (i + 1) + ":");
+            System.out.print("Name: ");
+            String name = scanner.next();
+
+            System.out.print("Author: ");
+            String author = scanner.next();
+
+            System.out.print("Price: $");
+            double price = scanner.nextDouble();
+
+            System.out.print("Number of Pages: ");
+            int numPages = scanner.nextInt();
+
+            books[i] = new Book(name, author, price, numPages);
+        }
+
+        System.out.println("\nDetails of all books:");
+        for (Book book : books) {
+            System.out.println(book.toString() + "\n");
+        }
+    }
+}
